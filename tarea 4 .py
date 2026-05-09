478a18c705b6572ce4ceac14f4d140c8e133ce60
juanveloza07-pixel <juanveloza07@gmail.com>
2026-05-08 20:55:14 -0500

    Clases Principales parte1

--- tarea 4 .py
+++ tarea 4 .py
@@ -15,3 +15,34 @@ class ReservaError(Exception):
 class ValidacionDatoError(ReservaError):
     """Error cuando un dato ingresado no cumple el formato o rango."""
     pass
+class Cliente:
+    def __init__(self, nombre, id_cliente):
+        self.__nombre = nombre  # Encapsulamiento
+        self.__id_cliente = id_cliente
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+class Servicio(ABC):
+    """Clase abstracta que demuestra herencia y polimorfismo."""
+    def __init__(self, nombre_servicio, costo_base):
+        self._nombre_servicio = nombre_servicio
+        self._costo_base = costo_base
+
+    @abstractmethod
+    def calcular_costo(self):
+        """Método abstracto para ser sobrescrito (Polimorfismo)."""
+        pass
+
+class ReservaSala(Servicio):
+    def __init__(self, horas):
+        super().__init__("Reserva de Sala", 50.0)
+        if horas <= 0:
+            raise ValidacionDatoError("Las horas deben ser mayores a cero.")
+        self.__horas = horas
+
+    def calcular_costo(self):
+        # Sobrescritura de método
+        return self._costo_base * self.__horas
+
